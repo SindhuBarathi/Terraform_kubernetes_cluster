d813bd8568af4e052bc772d1db2d2739c2165833
SindhuBarathi <sindhubarathiak@gmail.com>
2021-12-21 15:43:33 +0000

    completed

---
+++ module/App/output.tf
@@ -0,0 +1,4 @@
+# output "Kubernets_self_link" {
+#   description = "Name (id) of the bucket"
+#   value       = module.App.kuberents_self_links
+# }
---
+++ module/App/variable.tf
@@ -0,0 +1,21 @@
+variable "name" { 
+  type        = string
+}
+
+variable "project" {
+  type        = string
+  
+}
+
+variable "location" {
+  type        = string
+ 
+}
+variable "initial_node_count" {
+  type        = string
+  
+}
+variable "machine_type" {
+  type        = string
+  
+}
